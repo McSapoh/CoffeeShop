905fe58ba8ec9879fa0b8abaf9ab0fbec2f7088c
McSapoh <bodyamelnyk04@gmail.com>
2022-12-09 23:49:06 +0200

    Добавьте файлы проекта.

---
+++ CoffeeShopAPI/Models/CoffeeShopContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShopAPI.Models
+{
+    public class CoffeeShopContext : DbContext
+    {
+        public CoffeeShopContext(DbContextOptions<CoffeeShopContext> options) : base(options) {}
+    }
+}
