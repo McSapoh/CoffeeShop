ae9645ba4f62d018f45c312bb4feee8d71ed3b99
McSapoh <bodyamelnyk04@gmail.com>
2022-12-19 22:38:00 +0200

    Added field ICollection of Sizes to Product model

--- CoffeeShopAPI/Models/Product.cs
+++ CoffeeShopAPI/Models/Product.cs
@@ -1,9 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CoffeeShopAPI.Models
 {
     public class Product
     {
+        // Database columns.
         [Key]
         public int Id { get; set; }
         [Required(ErrorMessage = "Field Name cannot be empty")]
@@ -13,5 +15,9 @@ namespace CoffeeShopAPI.Models
         public ProductType ProductType { get; set; }
         public string ImagePath { get; set; }
         public bool IsActive { get; set; } = true;
+
+        // Objects for relationships
+        public virtual User User { get; set; }
+        public virtual ICollection<Size> Sizes { get; set; }
     }
 }
