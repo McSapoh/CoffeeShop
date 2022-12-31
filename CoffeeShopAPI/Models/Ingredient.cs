217bddd8db291c0c357fc11efd1fd5f5d619854b
McSapoh <bodyamelnyk04@gmail.com>
2022-12-31 11:29:32 +0200

    Added Collection of ProductOrders to Ingredient model

--- CoffeeShopAPI/Models/Ingredient.cs
+++ CoffeeShopAPI/Models/Ingredient.cs
@@ -1,9 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CoffeeShopAPI.Models
 {
     public class Ingredient
     {
+        #region Database columns
         [Key]
         public int Id { get; set; }
         [Required(ErrorMessage = "Field Name cannot be empty")]
@@ -12,5 +14,9 @@ namespace CoffeeShopAPI.Models
         public int Price { get; set; }
         public string IngredientType { get; set; }
         public bool IsActive { get; set; } = true;
+        #endregion
+        #region Objects for relationships
+        public virtual ICollection<ProductOrder> ProductOrders { get; set; }
+        #endregion
     }
 }
