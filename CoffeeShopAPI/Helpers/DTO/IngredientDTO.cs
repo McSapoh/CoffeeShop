1589f0914a2c6a3ec43e170ce0ff49fda073add5
McSapoh <bodyamelnyk04@gmail.com>
2022-12-31 21:14:40 +0200

    Created IngredientDTO

---
+++ CoffeeShopAPI/Helpers/DTO/IngredientDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShopAPI.Helpers.DTO
+{
+    public class IngredientDTO
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Field Name cannot be empty")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Field Price cannot be empty")]
+        public int Price { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
