9b9494bee931c1ac09b947330b665da3109b5094
McSapoh <bodyamelnyk04@gmail.com>
2023-01-04 12:23:16 +0200

    Created and overridden all necessary methods in IngredientRepository

--- CoffeeShopAPI/Interfaces/Repositories/IIngredientRepository.cs
+++ CoffeeShopAPI/Interfaces/Repositories/IIngredientRepository.cs
@@ -1,8 +1,10 @@
-﻿using CoffeeShopAPI.Models;
+﻿using CoffeeShopAPI.Helpers.Paging;
+using CoffeeShopAPI.Models;
 
 namespace CoffeeShopAPI.Interfaces.Repositories
 {
     public interface IIngredientRepository : IRepository<Ingredient>
     {
+        public PagedList<Ingredient> GetPagedList(PagingParameters pagingParameters, string type);
     }
 }
--- CoffeeShopAPI/Repositories/IngredientRepository.cs
+++ CoffeeShopAPI/Repositories/IngredientRepository.cs
@@ -1,5 +1,9 @@
-﻿using CoffeeShopAPI.Interfaces.Repositories;
+﻿using CoffeeShopAPI.Helpers.Paging;
+using CoffeeShopAPI.Interfaces.Repositories;
 using CoffeeShopAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace CoffeeShopAPI.Repositories
 {
@@ -8,6 +12,15 @@ namespace CoffeeShopAPI.Repositories
         public IngredientRepository(CoffeeShopContext db)
         {
             _dbSet = db.Set<Ingredient>();
+        }
+        public override Ingredient GetById(int Id) => _dbSet.First(p => p.Id == Id);
+        public async override Task<Ingredient> GetByIdAsync(int Id) => await _dbSet.FirstAsync(p => p.Id == Id);
+        public PagedList<Ingredient> GetPagedList(PagingParameters pagingParameters, string type)
+        {
+            var items = _dbSet.Where(o => o.IngredientType == type).Skip(
+                (pagingParameters.PageNumber - 1) * pagingParameters.PageSize).Take(pagingParameters.PageSize
+            ).ToList();
+            return new PagedList<Ingredient>(items, _dbSet.Count(), pagingParameters.PageSize, pagingParameters.PageSize);
         }
     }
 }
