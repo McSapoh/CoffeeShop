5633e7b090159a0609b4fca61d183ff89a901c04
McSapoh <bodyamelnyk04@gmail.com>
2023-01-10 15:38:27 +0200

    Created CreateUserDTO

---
+++ CoffeeShopAPI/Helpers/DTO/User/CreateUserDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShopAPI.Helpers.DTO.User
+{
+    public class CreateUserDTO
+    {
+        [Required(ErrorMessage = "Please enter Your Name")]
+        public string Name { get; set; }
+        [EmailAddress, Required(ErrorMessage = "Please enter Your email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter Your adress")]
+        public string Adress { get; set; }
+        [MinLength(4), Required(ErrorMessage = "Please enter Your password")]
+        public string Password { get; set; }
+    }
+}
