5c2e12c2cbb50f43971295c391699b8ef33dadce
McSapoh <bodyamelnyk04@gmail.com>
2023-01-12 18:55:41 +0200

    Created IImageService service and its implementation class ImageService

---
+++ CoffeeShopAPI/Helpers/Services/ImagesService.cs
@@ -0,0 +1,46 @@
+﻿using CoffeeShopAPI.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShopAPI.Helpers.Services
+{
+    public class ImagesService : IImagesService
+    {
+        public async Task<bool> SavePhoto(string path, IFormFile photo)
+        {
+            try
+            {
+                #region Getting random string
+                var random = new Random();
+                const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+                var randomSting =  new string(Enumerable.Repeat(chars, 12)
+                    .Select(s => s[random.Next(s.Length)]).ToArray());
+                #endregion
+                var fileName = randomSting + Path.GetExtension(photo.FileName);
+                string filePath = Path.Combine(path, fileName);
+                using Stream fileStream = new FileStream(filePath, FileMode.Create);
+                await photo.CopyToAsync(fileStream);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public bool DeletePhoto(string path)
+        {
+            try
+            {
+                File.Delete(path);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ CoffeeShopAPI/Interfaces/Services/IImagesService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace CoffeeShopAPI.Interfaces.Services
+{
+    public interface IImagesService
+    {
+        public Task<bool> SavePhoto(string path, IFormFile photo);
+        public bool DeletePhoto(string path);
+    }
+}
