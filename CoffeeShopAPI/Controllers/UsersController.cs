948a72de8d30ffdf947c0d7dbe0ee50d4550248a
McSapoh <bodyamelnyk04@gmail.com>
2023-01-12 14:30:27 +0200

    Created UsersController Created method CreateUser in UsersController (not fully completed)

---
+++ CoffeeShopAPI/Controllers/UsersController.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using CoffeeShopAPI.Helpers.DTO.User;
+using CoffeeShopAPI.Interfaces.Repositories;
+using CoffeeShopAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShopAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public UsersController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        [HttpPost("CreateUser")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> CreateUser([FromForm] CreateUserDTO objectFromPage, IFormFile photo)
+        {
+            if (photo != null && !photo.ContentType.Contains("image"))
+                return BadRequest(new JsonResult(new { success = false, message = $"File is not a photo" }));
+            if (ModelState.IsValid)
+            {
+                var user = _mapper.Map<User>(objectFromPage);
+                _unitOfWork.UserRepository.Create(user);
+                if (await _unitOfWork.SaveAsync())
+                    return Ok(new JsonResult(new
+                    {
+                        success = true,
+                        message = "Successfully saved"
+                    }));
+                else
+                    return BadRequest(new JsonResult(new
+                    {
+                        success = false,
+                        message = "Error while saving"
+                    }));
+            }
+            else
+                return BadRequest(ModelState);
+        }
+    }
+}
