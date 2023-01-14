800861c7102585046790ebfebc52668c8df8e50e
McSapoh <bodyamelnyk04@gmail.com>
2023-01-14 20:49:25 +0200

    Created UpdateUserDTO

---
+++ CoffeeShopAPI/Helpers/DTO/User/UpdateUserDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShopAPI.Helpers.DTO.User
+{
+    public class UpdateUserDTO
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter Your Name")]
+        public string Name { get; set; }
+        [EmailAddress, Required(ErrorMessage = "Please enter Your email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter Your adress")]
+        public string Adress { get; set; }
+        [MinLength(4), Required(ErrorMessage = "Please enter Your password")]
+        public string Password { get; set; }
+    }
+}
