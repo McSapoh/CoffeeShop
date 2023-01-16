de41b288903a4c6aeb1895eb3f778b79af9964e4
McSapoh <bodyamelnyk04@gmail.com>
2023-01-16 15:05:21 +0200

    Created DisplayUserDTO

---
+++ CoffeeShopAPI/Helpers/DTO/User/DisplayUserDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CoffeeShopAPI.Helpers.DTO.User
+{
+    public class DisplayUserDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Adress { get; set; }
+        public string ImagePath { get; set; }
+        public DateTime RegistrationDate { get; set; }
+    }
+}
