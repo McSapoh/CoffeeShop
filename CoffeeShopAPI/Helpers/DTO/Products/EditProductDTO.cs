d666ff0627a7138e9dcda95430182cfbfb2a82a5
McSapoh <bodyamelnyk04@gmail.com>
2023-01-22 16:24:37 +0200

    Created EditProductDTO

---
+++ CoffeeShopAPI/Helpers/DTO/Products/EditProductDTO.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShopAPI.Helpers.DTO.Products
+{
+    public class EditProductDTO
+    {
+        [Required(ErrorMessage = "Field Name cannot be empty")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Field Description cannot be empty")]
+        public string Description { get; set; }
+        public bool IsActive { get; set; } = true;
+        [MinLength(1), MaxLength(5)]
+        public virtual ICollection<SizeDTO> Sizes { get; set; }
+    }
+}
