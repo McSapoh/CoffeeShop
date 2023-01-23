8ef1d128342741e43b76944b7992a39bdb91d3cc
McSapoh <bodyamelnyk04@gmail.com>
2023-01-23 13:36:37 +0200

    Created CofffesController inheritated from ProductsController

---
+++ CoffeeShopAPI/Controllers/Products/CoffeesController.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using CoffeeShopAPI.Interfaces.Repositories;
+using CoffeeShopAPI.Interfaces.Services;
+using CoffeeShopAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShopAPI.Controllers.Products
+{
+    [Route("api/[controller]")]
+    [ApiExplorerSettings(IgnoreApi = false)]
+    public class CoffeesController : ProductsController
+    {
+        public CoffeesController(IUnitOfWork unitOfWork, IProductService productService, IMapper mapper) : 
+            base(unitOfWork, productService, mapper, ProductType.coffee)
+        { }
+    }
+}
