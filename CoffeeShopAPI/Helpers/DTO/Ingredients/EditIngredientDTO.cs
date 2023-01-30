34eadb7cc384950d087fd293841ea28006538cfa
McSapoh <bodyamelnyk04@gmail.com>
2023-01-30 18:25:27 +0200

    CreatedEditIngredientDTO

---
+++ CoffeeShopAPI/Helpers/DTO/Ingredients/EditIngredientDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShopAPI.Helpers.DTO.Ingredients
+{
+    public class EditIngredientDTO
+    {
+        [Required(ErrorMessage = "Field Name cannot be empty")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Field Price cannot be empty")]
+        public int Price { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
