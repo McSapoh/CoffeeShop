53d57587308a0158d93c9a41150ee2b32182cadb
McSapoh <bodyamelnyk04@gmail.com>
2023-02-01 19:16:16 +0200

    Add Id field to RefreshToken model

--- CoffeeShopAPI/Models/RefreshToken.cs
+++ CoffeeShopAPI/Models/RefreshToken.cs
@@ -4,6 +4,7 @@ namespace CoffeeShopAPI.Models
 {
     public class RefreshToken
     {
+        public int Id { get; set; }
         public string Token { get; set; } = string.Empty;
         public DateTime Created { get; set; } = DateTime.Now;
         public DateTime Expires { get; set; }
