9862d9228356f672f7df962a0edb4ea467b16932
McSapoh <bodyamelnyk04@gmail.com>
2023-02-01 19:12:51 +0200

    Creat RefreshToken model

---
+++ CoffeeShopAPI/Models/RefreshToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CoffeeShopAPI.Models
+{
+    public class RefreshToken
+    {
+        public string Token { get; set; } = string.Empty;
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime Expires { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
