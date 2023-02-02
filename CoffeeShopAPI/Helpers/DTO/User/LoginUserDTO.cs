edefc5197cd6099ab722f8dab4e872d7d17f8143
McSapoh <bodyamelnyk04@gmail.com>
2023-02-02 10:07:09 +0200

    Create LoginUserDTO

---
+++ CoffeeShopAPI/Helpers/DTO/User/LoginUserDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShopAPI.Helpers.DTO.User
+{
+    public class LoginUserDTO
+    {
+        [EmailAddress, Required(ErrorMessage = "Please enter Your phone number")]
+        public string Email { get; set; }
+        [MinLength(4), Required(ErrorMessage = "Please enter Your password")]
+        public string Password { get; set; }
+    }
+}
