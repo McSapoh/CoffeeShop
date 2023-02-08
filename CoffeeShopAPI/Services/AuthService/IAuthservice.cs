0e0937148c68c9aeda5fc717e64accb17277145c
McSapoh <bodyamelnyk04@gmail.com>
2023-02-08 17:59:23 +0200

    Fixed git errors

--- CoffeeShopAPI/Services/AuthService/IAuthservice.cs
+++ CoffeeShopAPI/Services/AuthService/IAuthservice.cs
@@ -1,6 +1,14 @@
-﻿namespace CoffeeShopAPI.Services
+﻿using CoffeeShopAPI.Models;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace CoffeeShopAPI.Services
 {
     public interface IAuthService
     {
+        public Task<User> GetUserByIdentity(HttpContext context);
+        public string GenerateJWT(User user);
+        public RefreshToken GenerateRefreshToken();
+        public void AppendRefreshTokenToResponse(RefreshToken newRefreshToken);
     }
 }
