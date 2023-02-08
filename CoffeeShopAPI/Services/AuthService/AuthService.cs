8589716e5799ca72e1fe333ba283c1946457d4e0
McSapoh <bodyamelnyk04@gmail.com>
2023-02-08 13:48:16 +0200

    Created necessary methods for Authentication and Authorization in AuthService

--- CoffeeShopAPI/Services/AuthService/AuthService.cs
+++ CoffeeShopAPI/Services/AuthService/AuthService.cs
@@ -1,6 +1,116 @@
-﻿namespace CoffeeShopAPI.Services
+﻿using CoffeeShopAPI.Models;
+using CoffeeShopAPI.UnitOfWork;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShopAPI.Services
 {
-    public class AuthService : IAuthService
+    public class AuthService : ControllerBase, IAuthService
     {
+        private readonly IConfiguration _config;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<AuthService> _logger;
+
+        public AuthService(IConfiguration config, IUnitOfWork unitOfWork, 
+            ILogger<AuthService> logger)
+        {
+            _config = config;
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+        }
+        public async Task<User> GetUserByIdentity(HttpContext context)
+        {
+            _logger.LogInformation($"{this}.GetUser called.");
+
+            // Getting identity from HttpContext.
+            var identity = context.User.Identity as ClaimsIdentity;
+
+            // Getting user by claims
+            var user = await _unitOfWork.UserRepository.GetByEmail(
+                identity.Claims.FirstOrDefault(u => u.Type == ClaimTypes.Email).Value
+            );
+
+            _logger.LogInformation($"{this}.GetUser finished with result {user}.");
+            return user;
+        }
+
+        public string GenerateJWT(User user)
+        {
+            _logger.LogInformation($"{this}.GenerateJWT called.");
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Auth:Key"]));
+            var credentias = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            // Building Claims array
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, user.Name),
+                new Claim(ClaimTypes.Email, user.Email),
+            };
+
+            // Building JwtSecurityToken.
+            var token = new JwtSecurityToken(
+                _config["Auth:Issuer"],
+                _config["Auth:Audience"],
+                claims,
+                expires: DateTime.Now.AddHours(Convert.ToDouble(_config["Auth:JWTExpires"])),
+                signingCredentials: credentias
+            );
+
+            _logger.LogInformation($"{this}.GenerateJWT JwtsevurityToken was build.");
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public RefreshToken GenerateRefreshToken()
+        {
+            _logger.LogInformation($"{this}.GenerateRefreshToken called.");
+            string token;
+
+            // Creating base64 encoded random security token.
+            using (RandomNumberGenerator rng = new RNGCryptoServiceProvider())
+            {
+                byte[] tokenData = new byte[32];
+                rng.GetBytes(tokenData);
+
+                token = Convert.ToBase64String(tokenData);
+            }
+
+            // Building RefreshToken.
+            var refreshToken = new RefreshToken
+            {
+                Token = token,
+                Created = DateTime.Now,
+                Expires = DateTime.Now.AddDays(double.Parse(_config["Auth:RefreshExpires"])),
+            };
+
+            _logger.LogInformation($"{this}.GenerateRefreshToken finished.");
+            return refreshToken;
+        }
+
+        public void AppendRefreshTokenToResponse(RefreshToken newRefreshToken)
+        {
+            _logger.LogInformation($"{this}.AppendRefreshTokenToResponse called.");
+
+            // Setting cookies.
+            var cookieOptions = new CookieOptions
+            {
+                HttpOnly = true,
+                Expires = newRefreshToken.Expires
+            };
+
+            // Appending data to response.
+            Response.Cookies.Append("refreshToken", newRefreshToken.Token, cookieOptions);
+            _logger.LogInformation($"{this}.AppendRefreshTokenToResponse finished.");
+        }
     }
 }
---
+++ CoffeeShopAPI/Services/AuthService/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using CoffeeShopAPI.Models;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace CoffeeShopAPI.Services
+{
+    public interface IAuthService
+    {
+        public Task<User> GetUserByIdentity(HttpContext context);
+        public string GenerateJWT(User user);
+        public RefreshToken GenerateRefreshToken();
+        public void AppendRefreshTokenToResponse(RefreshToken newRefreshToken);
+    }
+}
