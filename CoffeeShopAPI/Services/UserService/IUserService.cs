a5cb184397af522d9517d541c1267e512a4c51ef
McSapoh <bodyamelnyk04@gmail.com>
2023-02-09 10:06:11 +0200

    Removed all auth methods from IUserService and UserService

--- CoffeeShopAPI/Services/UserService/IUserService.cs
+++ CoffeeShopAPI/Services/UserService/IUserService.cs
@@ -9,9 +9,5 @@ namespace CoffeeShopAPI.Services
     {
         public Task<IActionResult> Create(User user, IFormFile photo);
         public Task<IActionResult> Update(User user, IFormFile photo);
-        public Task<User> GetUserByIdentity(HttpContext context);
-        public string GenerateJWT(User user);
-        public RefreshToken GenerateRefreshToken();
-        public void AppendRefreshTokenToResponse(RefreshToken newRefreshToken);
     }
 }
