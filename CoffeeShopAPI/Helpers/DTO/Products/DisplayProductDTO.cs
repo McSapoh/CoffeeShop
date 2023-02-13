255f1c974172781462a32294b2eefb7dc1ef2235
McSapoh <bodyamelnyk04@gmail.com>
2023-02-13 18:22:08 +0200

    Created DisplayProductDTO

---
+++ CoffeeShopAPI/Helpers/DTO/Products/DisplayProductDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace CoffeeShopAPI.Helpers.DTO.Products
+{
+    public class DisplayProductDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
