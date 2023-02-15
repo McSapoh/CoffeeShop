29e8ee48bf55e180d265259745d1a69ee887da00
McSapoh <bodyamelnyk04@gmail.com>
2023-02-15 23:21:36 +0200

    Created EmailDTO

---
+++ CoffeeShopAPI/Helpers/DTO/Email/EmailDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace CoffeeShopAPI.Helpers.DTO.Email
+{
+    public class EmailDTO
+    {
+        public string ToEmail { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
