4bc6d3d5f67cd906fecb609e7c7518882d30f5ca
McSapoh <bodyamelnyk04@gmail.com>
2023-02-21 18:08:40 +0200

    Fixing doubling IAuthService (restored IAuthService)

---
+++ CoffeeShopAPI/Services/AuthService/IAuthService.cs
@@ -0,0 +1,17 @@
+﻿using CoffeeShopAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CoffeeShopAPI.Services
+{
+    public interface IAuthService
+    {
+        public Task<User> GetUserByIdentity(HttpContext context);
+        public string GenerateJWT(User user);
+        public RefreshToken GenerateRefreshToken();
+        public string GenerateRandomToken();
+        public void AppendRefreshTokenToResponse(RefreshToken newRefreshToken, HttpResponse response);
+        public Task<IActionResult> ConfirmEmail(ConfirmEmailToken confirmEmailToken);
+    }
+}
