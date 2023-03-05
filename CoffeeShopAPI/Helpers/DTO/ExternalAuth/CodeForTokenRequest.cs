5591bc441712ddcd8a4d8b428de17148de8a5579
McSapoh <bodyamelnyk04@gmail.com>
2023-03-05 16:52:19 +0200

    Created classes for external authentication requests

---
+++ CoffeeShopAPI/Helpers/DTO/ExternalAuth/CodeForTokenRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace CoffeeShopAPI.Helpers.DTO
+{
+    public class CodeForTokenRequest
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string Code { get; set; }
+        public string CodeVerifier { get; set; }
+        public string RedirectUrl { get; set; }
+        public string PostUrl { get; set; }
+    }
+}
---
+++ CoffeeShopAPI/Helpers/DTO/ExternalAuth/ExternalAuthorizationRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace CoffeeShopAPI.Helpers.DTO
+{
+    public class ExternalAuthorizationRequest
+    {
+        public string ClientId { get; set; }
+        public string RedirectUrl { get; set; }
+        public string Scope { get; set; }
+        public string CodeChallenge { get; set; }
+        public string Uri { get; set; }
+    }
+}
